6a759e580f75874eced4c255cc63bbc775570683
rahulrudra81 <rahulrudra81@gmail.com>
2024-05-08 00:27:17 +0530

    changed mapping

--- src/utils/openAI/textToSpeech.js
+++ src/utils/openAI/textToSpeech.js
@@ -6,16 +6,17 @@ import { uplaodOnCloudinary } from "../cloudinary.js";
 const speechile =  path.resolve(`./public/temp/${Date()}.mp3`)
 
 export const convertTextToSpeech = async(inputText ,voice ) =>{
-    // const mp3 = await openAi.audio.speech.create({
-    //     model : "tts-1",
-    //     voice : voice,
-    //     input : inputText
-    // })
-    // const buffer = Buffer.from(await mp3.arrayBuffer())
-    // await fs.promises.writeFile(speechile , buffer)
-    // const cloudinaryResponse = await uplaodOnCloudinary(speechile);
-    // console.log("Text to voice : ", {inputText , voice});
-    // return cloudinaryResponse.url;
-    const res = `this is voice response ${inputText} ${voice}`
-    return res
+    const mp3 = await openAi.audio.speech.create({
+        model : "tts-1",
+        voice : voice,
+        input : inputText
+    })
+    const buffer = Buffer.from(await mp3.arrayBuffer())
+    await fs.promises.writeFile(speechile , buffer)
+    const cloudinaryResponse = await uplaodOnCloudinary(speechile);
+    console.log("Text to voice : ", {inputText , voice});
+    return cloudinaryResponse.url;
+    // const res = `this is voice response ${inputText} ${voice}`
+    // return res
 }   
+
--- src/utils/openAI/useModels.js
+++ src/utils/openAI/useModels.js
@@ -1,70 +1,56 @@
-import { gptGeneration ,textToSpeech } from "../../controllers/openAI.controller.js";
+import { gptGeneration, textToSpeech } from "../../controllers/openAI.controller.js";
 import textToImage from "./textToImage.js";
-import async from 'async'
 
-
-const callFunction1 = async (path,nodeInfo , input, i ,output) => {
+const callFunction1 = async (path, nodeInfo, input, i, output) => {
     if (nodeInfo[path[i]].functionCode === 'input') {
-        output.push({ type: 'input', value: input })
-        
-        return input
-    }
-    else if (nodeInfo[path[i]].functionCode === 'gptNode') {
-        const gptResponse = await gptGeneration(input , nodeInfo[path[i]].data);
-        output.push({type:'gptResponse', value: gptResponse})
-        // console.log(output)
-        
-        return gptResponse;
-    }
-
-    else if (nodeInfo[path[i]].functionCode === 'TextToAudio') {
-        const speechFile = await textToSpeech(input , nodeInfo[path[i]].data);
-        output.push({type:'speechFile',value:speechFile})
-        return speechFile;
-    }
-    else if (nodeInfo[path[i]].functionCode === 'output') {
-        output.push({type:'output', value: input})
+        output.push({ type: 'input', value: input });
         return input;
-    }
-    else if(nodeInfo[path[i]].functionCode === 'TextToImage'){
-        const fileUrl = await textToImage(input , nodeInfo[path[i]].data)
-        output.push({type : 'imageUrl' , value : fileUrl})
+    } else if (nodeInfo[path[i]].functionCode === 'gptNode') {
+        const gptResponse = await gptGeneration(input, nodeInfo[path[i]].data);
+        output.push({ type: 'gptResponse', value: gptResponse });
+        return gptResponse;
+    } else if (nodeInfo[path[i]].functionCode === 'TextToAudio') {
+        const speechFile = await textToSpeech(input, nodeInfo[path[i]].data);
+        output.push({ type: 'speechFile', value: speechFile });
+        return speechFile;
+    } else if (nodeInfo[path[i]].functionCode === 'output') {
+        output.push({ type: 'output', value: input });
+        return input;
+    } else if (nodeInfo[path[i]].functionCode === 'TextToImage') {
+        const fileUrl = await textToImage(input, nodeInfo[path[i]].data);
+        output.push({ type: 'imageUrl', value: fileUrl });
         return fileUrl;
     }
-    
-}
+};
 
-
-const callFunction = async(path , nodeInfo , inputText , res)=>{
+const callFunction = async (path, nodeInfo, inputText) => {
     const output = [];
+    let cur = inputText;
     
-    var cur =await  callFunction1(path , nodeInfo , inputText , 0 , output)
-    for(let i=1 ; i<path.length ; i++){
-        const currentCall = await  callFunction1(path , nodeInfo , cur, i, output ) 
-        cur=currentCall
-        // console.log({i , output});
+    for (let i = 0; i < path.length; i++) {
+        cur = await callFunction1(path, nodeInfo, cur, i, output);
     }
-    console.log({output})
-    res.push(output)
-    // console.log(output);
-}
+    
+    return output;
+};
 
-const usingModels = async(modelFlow ,nodeInfo, inputText) => {
+const usingModels = async (modelFlow, nodeInfo, inputText) => {
     try {
         const functionArray = [];
-        const res = [];
-        for(let i=0 ; i<modelFlow.length ; i++){
-            functionArray.push( callFunction(modelFlow[i] , nodeInfo , inputText , res))
-        }
-        console.log({functionArray})
-        const calls = await async.parallel(functionArray);
         
-        console.log({calls});
-        return calls
+        for (let i = 0; i < modelFlow.length; i++) {
+            // Push actual async function calls into the array
+            functionArray.push(callFunction(modelFlow[i], nodeInfo, inputText));
+        }
+        
+        const calls = await Promise.all(functionArray);
+        console.log({ calls });
+        return calls;
         
     } catch (error) {
-        return error
+        console.error('Error in usingModels:', error);
+        throw error;
     }
-}
+};
 
-export default usingModels
+export default usingModels;
